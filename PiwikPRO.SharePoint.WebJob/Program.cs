9c86dc761e92db55308e7575e8c2ab06338e5c97
Lukasz Kolodziejczak <lkolodziejczak@kogifi.com>
2020-02-07 10:04:42 +0000

    removed connection string key in cs function

--- PiwikPRO.SharePoint.WebJob/Program.cs
+++ PiwikPRO.SharePoint.WebJob/Program.cs
@@ -17,8 +17,6 @@ namespace PiwikPRO.SharePoint.WebJob
             }
 
             config.UseTimers();
-            config.DashboardConnectionString = "DefaultEndpointsProtocol=https;AccountName=kogifilist8d24;AccountKey=BNBaVPh1sCHICO3P40LHpaTKnD8KKwgbZfLRIIILK3GP4L15/Sy8+0ZkEB/wrtqwyi0xlWLGwsjjD/JR2e7+1A==;EndpointSuffix=core.windows.net";
-            config.StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=kogifilist8d24;AccountKey=BNBaVPh1sCHICO3P40LHpaTKnD8KKwgbZfLRIIILK3GP4L15/Sy8+0ZkEB/wrtqwyi0xlWLGwsjjD/JR2e7+1A==;EndpointSuffix=core.windows.net";
 
             var host = new JobHost(config);
             // The following code ensures that the WebJob will be running continuously
