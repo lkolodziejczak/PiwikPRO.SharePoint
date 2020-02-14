6c5298aaf50f779cbbec4f4fd6ad434bfd434139
Paweł Hawrylak <phawrylak@kogifi.com>
2020-02-14 01:39:29 +0100

    Initial commit for Tests project

---
+++ PiwikPRO.SharePoint.Tests/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Threading;
+
+namespace PiwikPRO.SharePoint.Tests
+{
+    public class SampleTest : SeleniumTestFixture
+    {
+        public SampleTest(string driverName) : base(driverName)
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Driver.Url = "https://www.google.pl";
+            Thread.Sleep(10000);
+        }
+    }
+}
