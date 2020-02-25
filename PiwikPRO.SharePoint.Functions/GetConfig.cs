00024808cb085245ca6f5ef379783cdc9705fbd1
Lukasz Kolodziejczak <lkolodziejczak@kogifi.com>
2020-02-25 21:00:06 +0000

    added monitoring to azure function

--- PiwikPRO.SharePoint.Functions/GetConfig.cs
+++ PiwikPRO.SharePoint.Functions/GetConfig.cs
@@ -21,25 +21,29 @@ namespace PiwikPRO.SharePoint.Functions
             ILogger log,
             ExecutionContext context)
         {
+            Monitoring monitoring = new Monitoring();
+
+            JObject piwikConfig = new JObject();
+
             if (req == null)
                 throw new ArgumentNullException(nameof(req));
             if (context == null)
                 throw new ArgumentNullException(nameof(context));
 
-            var config = new ConfigurationBuilder()
-                .SetBasePath(context.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(context.FunctionAppDirectory)
+                    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
 
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(config["AzureWebJobsStorage"]);
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            CloudBlobContainer container = blobClient.GetContainerReference("piwikblob");
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(config["AzureWebJobsStorage"]);
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("piwikblob");
 
-            CloudBlockBlob baseConfigBlob = container.GetBlockBlobReference("piwik-config.json");
+                CloudBlockBlob baseConfigBlob = container.GetBlockBlobReference("piwik-config.json");
 
-            string configJson = await baseConfigBlob.DownloadTextAsync();
-            JObject piwikConfig = JObject.Parse(configJson);
+                string configJson = await baseConfigBlob.DownloadTextAsync();
+                piwikConfig = JObject.Parse(configJson);
 
             if (req.Query["extension"] != StringValues.Empty)
             {
@@ -51,9 +55,9 @@ namespace PiwikPRO.SharePoint.Functions
                         string extensionConfig = await extensionConfigBlob.DownloadTextAsync();
                         piwikConfig.Merge(JObject.Parse(extensionConfig), new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Union });
                     }
-                    catch (StorageException)
+                    catch (StorageException exs)
                     {
-                        // Ignore
+                        monitoring.WriteLog("Storage", "GetConfig Storage exception - extension", exs.Message);
                     }
                 }
             }
@@ -66,14 +70,15 @@ namespace PiwikPRO.SharePoint.Functions
                     string tenantConfigJson = await tenantConfigBlob.DownloadTextAsync();
                     piwikConfig.Merge(JObject.Parse(tenantConfigJson), new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Union });
                 }
-                catch (StorageException)
+                catch (StorageException exs)
                 {
-                    // Ignore
+                    monitoring.WriteLog("Storage", "GetConfig Storage exception - tenant", exs.Message);
                 }
             }
 
             req.HttpContext.Response.Headers["Cache-Control"] = "max-age=86400";
-            return new OkObjectResult(piwikConfig.ToString());
+
+            return new OkObjectResult(Convert.ToString(piwikConfig));
         }
     }
 }
---
+++ PiwikPRO.SharePoint.Functions/Monitoring.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiwikPRO.SharePoint.Functions
+{
+    public class Monitoring
+    {
+        public string DiagnosticAreaName = "PiwikPROConfigServer";
+        public void WriteLog(string eventName, string source, string errorMessage)
+        {
+            TelemetryConfiguration.Active.TelemetryChannel.DeveloperMode = true;
+
+            var tc = new TelemetryClient();
+            tc.TrackRequest(source + " : " + errorMessage, DateTimeOffset.UtcNow, new TimeSpan(0, 0, 3), "200", true);
+            tc.TrackMetric(DiagnosticAreaName, 100);
+            tc.TrackEvent(eventName);
+
+            tc.Flush();
+        }
+    }
+}
