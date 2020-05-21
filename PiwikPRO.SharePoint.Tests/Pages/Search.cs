6fc833956245a43065f7256cb22446b5c9b9f75b
Lukasz Kolodziejczak <lkolodziejczak@kogifi.com>
2020-05-21 20:17:34 +0000

    added search tests

---
+++ PiwikPRO.SharePoint.Tests/Pages/Search.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PiwikPRO.SharePoint.Tests.Pages
+{
+    class Search
+    {
+        private IWebDriver driver;
+        [FindsBy(How = How.XPath, Using = "//input[@type='search'][@role='combobox']")]
+        private IWebElement searchInput;
+
+        public Search(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void SearchFromTopSearch()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@type='search'][@role='combobox']")));
+            searchInput.SendKeys("a");
+            Thread.Sleep(500);
+            searchInput.SendKeys(Keys.Enter);
+        }
+    }
+}
---
+++ PiwikPRO.SharePoint.Tests/SeleniumTests/SearchTests.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using PiwikPRO.SharePoint.Tests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PiwikPRO.SharePoint.Tests.SeleniumTests
+{
+    class SearchTests : TestBase
+    {
+        IJavaScriptExecutor jse;
+        Search search;
+        [OneTimeSetUp]
+        public void BeforeTest()
+        {
+            var loginPage = new LoginPage(_webDriver);
+            jse = (IJavaScriptExecutor)_webDriver;
+            loginPage.LoginToSharepoint(sharepointUserToTest, sharepointUserPasswordToTest);
+            search = new Search(_webDriver);
+        }
+        [SetUp]
+        public void ForEachTest()
+        {
+            _webDriver.Navigate().GoToUrl(homePage);
+            Thread.Sleep(1500);
+        }
+
+        [Test]
+        public void SearchInputType()
+        {
+            search.SearchFromTopSearch();
+            {
+                var search2 = new Search(_webDriver);
+                IJavaScriptExecutor jse = (IJavaScriptExecutor)_webDriver;
+                Thread.Sleep(2000);
+
+                object searchEvent = null;
+                string count = null;
+                string queryString = null;
+                string userLogin = null;
+
+                for (int i = 0; i < 30; i++)
+                {
+                    searchEvent = jse.ExecuteScript("return dataLayer.find(x => x.event === 'search')");
+                    if (search != null)
+                    {
+                        var json = JsonConvert.SerializeObject(searchEvent);
+                        Dictionary<string, string> dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                        dictionary.TryGetValue("count", out count);
+                        dictionary.TryGetValue("queryString", out queryString);
+                        dictionary.TryGetValue("userLogin", out userLogin);
+                        break;
+                    }
+                    Thread.Sleep(100);
+                }
+                Assert.NotNull(searchEvent);
+                Assert.NotNull(count);
+                Assert.NotNull(userLogin);
+            }
+        }
+    }
+}
