50348bfa72519386b7aff4d4a53c355afd92ad7a
Lukasz Kolodziejczak <lkolodziejczak@kogifi.com>
2020-06-04 09:53:26 +0000

    Added create classic list / library tests

---
+++ PiwikPRO.SharePoint.Tests/Pages/SharepointClassicLists.cs
@@ -0,0 +1,130 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using PiwikPRO.SharePoint.Tests.SeleniumTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PiwikPRO.SharePoint.Tests.Pages
+{
+    class SharepointClassicLists : TestBase
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = "//a[@name='add an app']")]
+        private IWebElement addAnAppButton;
+
+        [FindsBy(How = How.XPath, Using = "//div[@Title='" + resourceCustomList + "']")]
+        private IWebElement addAList;
+
+        [FindsBy(How = How.XPath, Using = "//div[@Title='" + resourceDocumentLibrary + "']")]
+        private IWebElement addALib;
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='onetidListTitle']")]
+        private IWebElement inputTextCreateList;
+
+        [FindsBy(How = How.XPath, Using = "//a[@aria-label='" + resourceBackToClassicSharepoint + "']")]
+        private IWebElement returnToClassicButton;
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='Cancel']")]
+        private IWebElement inputCancelButton;
+
+        public SharepointClassicLists(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void CreateClassicListFromSiteContents()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@aria-label='"+ resourceBackToClassicSharepoint+"']")));
+            returnToClassicButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@name='add an app']")));
+            addAnAppButton.Click();
+            Thread.Sleep(1000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@Title='" + resourceCustomList + "']")));
+            addAList.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//IFrame[contains(@id,'DlgFrame')]")));
+            string frameElementXpath = "//IFrame[contains(@id,'DlgFrame')]";
+            IWebElement f = driver.FindElement(By.XPath(frameElementXpath));
+            driver.SwitchTo().Frame(f);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@id='onetidListTitle']")));
+            inputTextCreateList.SendKeys(DateTime.Now.ToString("hhmm") + "ClassicTestList");
+            Thread.Sleep(500);
+            inputTextCreateList.SendKeys(Keys.Enter);
+        }
+
+        public void CreateClassicListFromSiteContentsAdvancedMode()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@aria-label='" + resourceBackToClassicSharepoint + "']")));
+            returnToClassicButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@name='add an app']")));
+            addAnAppButton.Click();
+            Thread.Sleep(1000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@Title='" + resourceCustomList + "']")));
+            addAList.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//IFrame[contains(@id,'DlgFrame')]")));
+            string frameElementXpath = "//IFrame[contains(@id,'DlgFrame')]";
+            IWebElement f = driver.FindElement(By.XPath(frameElementXpath));
+            driver.SwitchTo().Frame(f);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@id='onetidListTitle']")));
+            Thread.Sleep(1000);
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+            jse.ExecuteScript("NavigateToFullPage()");
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@id='onetidListTitle']")));
+            inputTextCreateList.SendKeys(DateTime.Now.ToString("hhmm") + "ClassicTestLibAdv");
+            Thread.Sleep(500);
+            inputTextCreateList.SendKeys(Keys.Enter);
+        }
+
+        public void CreateClassicLibraryFromSiteContents()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@aria-label='" + resourceBackToClassicSharepoint + "']")));
+            returnToClassicButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@name='add an app']")));
+            addAnAppButton.Click();
+            Thread.Sleep(1000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@Title='" + resourceDocumentLibrary + "']")));
+            addALib.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//IFrame[contains(@id,'DlgFrame')]")));
+            string frameElementXpath = "//IFrame[contains(@id,'DlgFrame')]";
+            IWebElement f = driver.FindElement(By.XPath(frameElementXpath));
+            driver.SwitchTo().Frame(f);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@id='onetidListTitle']")));
+            inputTextCreateList.SendKeys(DateTime.Now.ToString("hhmm") + "ClassicTestLib");
+            Thread.Sleep(500);
+            inputTextCreateList.SendKeys(Keys.Enter);
+        }
+
+        public void CreateClassicLibraryFromSiteContentsAdvancedMode()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@aria-label='" + resourceBackToClassicSharepoint + "']")));
+            returnToClassicButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@name='add an app']")));
+            addAnAppButton.Click();
+            Thread.Sleep(1000);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@Title='" + resourceDocumentLibrary + "']")));
+            addALib.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//IFrame[contains(@id,'DlgFrame')]")));
+            string frameElementXpath = "//IFrame[contains(@id,'DlgFrame')]";
+            IWebElement f = driver.FindElement(By.XPath(frameElementXpath));
+            driver.SwitchTo().Frame(f);
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@id='onetidListTitle']")));
+            Thread.Sleep(1000);
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+            jse.ExecuteScript("NavigateToFullPage()");
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@id='onetidListTitle']")));
+            inputTextCreateList.SendKeys(DateTime.Now.ToString("hhmm") + "ClassicTestLib");
+            Thread.Sleep(500);
+            inputTextCreateList.SendKeys(Keys.Enter);
+        }
+    }
+}
--- PiwikPRO.SharePoint.Tests/SeleniumTests/TestBase.cs
+++ PiwikPRO.SharePoint.Tests/SeleniumTests/TestBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -24,6 +25,12 @@ namespace PiwikPRO.SharePoint.Tests.SeleniumTests
         public string siteContents = "https://phkogifi.sharepoint.com/sites/PH/_layouts/15/viewlsts.aspx?view=14";
         public string homePage = "https://phkogifi.sharepoint.com/sites/PH/";
         public string listToTest = "https://phkogifi.sharepoint.com/sites/PH/Lists/testowa%20lista/AllItems.aspx";
+        public string siteContentsClassicToTest = "https://phkogifi.sharepoint.com/sites/PH/ClassicExp/_layouts/15/viewlsts.aspx?view=14";
+
+        //resources : to be changed on another language version of sharepoint:
+        public const string resourceBackToClassicSharepoint = "Click or enter to return to classic SharePoint";
+        public const string resourceCustomList = "Custom List";
+        public const string resourceDocumentLibrary = "Document Library";
 
         public IWebDriver _webDriver;
 
