8d17875045abb64c4b427e5fc1a195a7ac1006bc
Grzegorz Grudziński <ggrudzinski@kogifi.com>
2020-06-17 21:16:21 +0200

    added some tests

--- PiwikPRO.SharePoint.Tests/Pages/SharepointClassic.cs
+++ PiwikPRO.SharePoint.Tests/Pages/SharepointClassic.cs
@@ -38,11 +38,20 @@ namespace PiwikPRO.SharePoint.Tests.Classic
         [FindsBy(How = How.ClassName, Using = "ms-TextField-field")]
         private IWebElement newListPageNameInput;
 
+        [FindsBy(How = How.Id, Using = "ctl00_PlaceHolderMain_nameInput")]
+        private IWebElement newPageNameInput;
+
         [FindsBy(How = How.XPath, Using = "//i[@data-icon-name='CheckMark']")]
         private IWebElement showInLocalNavCheckbox;
 
         [FindsBy(How = How.ClassName, Using = "ms-ListCreationPanel-CreateButton")]
         private IWebElement newListButton;
+
+        [FindsBy(How = How.Id, Using = "ctl00_PlaceHolderMain_createButton")]
+        private IWebElement newPageButton;
+
+        [FindsBy(How = How.Id, Using = "Ribbon.EditingTools.CPEditTab.EditAndCheckout.SaveEdit-SelectedItem")]
+        private IWebElement saveOnRibbonPageButton;
 
         [FindsBy(How = How.CssSelector, Using = ".LeftNav-subLinksClip .LeftNav-notificationLink .LeftNav-linkText")]
         private IWebElement classicViewButton;
@@ -65,33 +74,24 @@ namespace PiwikPRO.SharePoint.Tests.Classic
         [FindsBy(How = How.XPath, Using = "//a[starts-with(@aria-label, 'Document')]")]
         private IWebElement fileItem;
 
+        [FindsBy(How = How.Id, Using = "WPQ1_ListTitleViewSelectorMenu_Container_surfaceopt1")]
+        private IWebElement allPagesButton;
+
+        [FindsBy(How = How.Id, Using = "btnShare")]
+        private IWebElement shareButton;
+
         public SharepointClassic(IWebDriver driver)
         {
             this.driver = driver;
             PageFactory.InitElements(driver, this);
         }
-
-        public void SwitchToClassicExp()
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".LeftNav-subLinksClip .LeftNav-notificationLink .LeftNav-linkText")));
-            classicViewButton.Click();
-        }
         public void ListItemDirectView()
         {
-            //string listName = this.NewListFromHomePageCreation();
-            //this.NewListItemCreation();
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            //wait.Until(ExpectedConditions.ElementIsVisible(By.Id("idHomePageNewItem")));
-            //classicViewButton.Click();
             Thread.Sleep(3000);
             var element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("tbody > tr > td > div > a")));
             Thread.Sleep(1000);
             element.Click();
-            //var attachmentItem = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#idAttachmentsTable > tbody > tr > td > span > a")));
-            //attachmentItem.Click();
-            //Thread.Sleep(1000);
-            // driver.Navigate().Back();
             Thread.Sleep(1000);
         }
         public void ListItemAttachmentView()
@@ -153,9 +153,9 @@ namespace PiwikPRO.SharePoint.Tests.Classic
             wait.Until(ExpectedConditions.ElementIsVisible(By.Id("ccfc_folderNameInput_0_onetidIOFile")));
             newFolderNameInput.Click();
             newFolderNameInput.SendKeys($"testfolder{random}");
-            Thread.Sleep(300);
-            newFolderCreateButton.Click();
             Thread.Sleep(1500);
+            newFolderCreateButton.Click();
+            Thread.Sleep(500);
             return $"testfolder{random}";
         }
         public void FileCreated()
@@ -184,6 +184,182 @@ namespace PiwikPRO.SharePoint.Tests.Classic
             wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
             fileItem.Click();
             driver.Navigate().Back();
+            Thread.Sleep(3000);
+        }
+        public string PageCreated()
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(999999);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            newButton.Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("ctl00_PlaceHolderMain_nameInput")));
+            newPageNameInput.Click();
+            newPageNameInput.SendKeys($"testPage{random}");
+            Thread.Sleep(500);
+            newPageButton.Click();
+            //wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("Ribbon.EditingTools.CPEditTab.EditAndCheckout.SaveEdit-SelectedItem")));
+            //saveOnRibbonPageButton.Click();
+            Thread.Sleep(3000);
+            return $"testPage{random}";
+        }
+        public string PageCreatedForPageAdditionalNeeds()
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(999999);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            newButton.Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("ctl00_PlaceHolderMain_nameInput")));
+            newPageNameInput.Click();
+            newPageNameInput.SendKeys($"testPage{random}");
+            Thread.Sleep(500);
+            newPageButton.Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("Ribbon.EditingTools.CPEditTab.EditAndCheckout.SaveEdit-SelectedItem")));
+            saveOnRibbonPageButton.Click();
+            Thread.Sleep(3000);
+            return $"testPage{random}";
+        }
+        public void PageSharedContextMenu(string pageName)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            allPagesButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            IWebElement page = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            Actions action = new Actions(driver);
+            action.ContextClick(page).Perform();
+            IWebElement shareMenuButton = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//li[@aria-label='Share']")));
+            action.MoveToElement(shareMenuButton);
+            shareMenuButton.Click();
+            IWebElement peoplePickerDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan")));
+            action.MoveToElement(peoplePickerDiv);
+            peoplePickerDiv.Click();
+            IWebElement peoplePickerInput = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_EditorInput")));
+            action.MoveToElement(peoplePickerInput);
+            Thread.Sleep(500);
+            peoplePickerInput.SendKeys("mzywicki");
+            IWebElement peoplePickerResultContainer = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_AutoFillDiv")));
+            peoplePickerResultContainer.Click();
+            shareButton.Click();
+            Thread.Sleep(3000);
+        }
+        public void PageSharedThreeDotsMenu(string pageName)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            allPagesButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            IWebElement page = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            Actions action = new Actions(driver);
+            IWebElement row = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]/../../..")));
+            IWebElement threeDotsButton = row.FindElement(By.XPath(".//a[contains(@id,'calloutLaunchPoint')]"));
+            threeDotsButton.Click();
+            IWebElement popUp =  wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[contains(@id,'callout-content')]")));
+            IWebElement threeDotsFromPopUp = popUp.FindElement(By.ClassName("js-callout-ecbActionDownArrow"));
+            threeDotsFromPopUp.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("ms-contextmenu-list")));
+            IWebElement menu = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//li[@aria-label='Share']")));
+            action.MoveToElement(menu);
+            menu.Click();
+            IWebElement peoplePickerDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan")));
+            action.MoveToElement(peoplePickerDiv);
+            peoplePickerDiv.Click();
+            IWebElement peoplePickerInput = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_EditorInput")));
+            action.MoveToElement(peoplePickerInput);
+            Thread.Sleep(500);
+            peoplePickerInput.SendKeys("mzywicki");
+            IWebElement peoplePickerResultContainer = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_AutoFillDiv")));
+            peoplePickerResultContainer.Click();
+            shareButton.Click();
+            Thread.Sleep(3000);
+        }
+        public void PageCopyLinkFromShareMenu(string pageName)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            allPagesButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            IWebElement page = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            Actions action = new Actions(driver);
+            action.ContextClick(page).Perform();
+            IWebElement shareMenuButton = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//li[@aria-label='Share']")));
+            action.MoveToElement(shareMenuButton);
+            shareMenuButton.Click();
+            Thread.Sleep(1500);
+            IWebElement getLinkButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("lnkGetLnkDlg")));
+            action.MoveToElement(getLinkButton);
+
+            getLinkButton.Click();
+            Thread.Sleep(3000);
+        }
+        public void PageCopyLinkFromContextMenu(string pageName)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            allPagesButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            IWebElement page = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{pageName}')]")));
+            Actions action = new Actions(driver);
+            action.ContextClick(page).Perform();
+            IWebElement getLinkButton = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//li[@aria-label='Get a link']")));
+            action.MoveToElement(getLinkButton);
+            getLinkButton.Click();
+            Thread.Sleep(3000);
+        }
+        public void FolderSharedContextMenu()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            string folderName = this.FolderCreated();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            //allPagesButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//a[starts-with(@aria-label,'{folderName}')]")));
+            IWebElement folder = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{folderName}')]")));
+            Actions action = new Actions(driver);
+            action.ContextClick(folder).Perform();
+            IWebElement shareMenuButton = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//li[@aria-label='Share']")));
+            action.MoveToElement(shareMenuButton);
+            shareMenuButton.Click();
+            IWebElement peoplePickerDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan")));
+            action.MoveToElement(peoplePickerDiv);
+            peoplePickerDiv.Click();
+            IWebElement peoplePickerInput = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_EditorInput")));
+            action.MoveToElement(peoplePickerInput);
+            Thread.Sleep(500);
+            peoplePickerInput.SendKeys("mzywicki");
+            IWebElement peoplePickerResultContainer = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_AutoFillDiv")));
+            peoplePickerResultContainer.Click();
+            shareButton.Click();
+            Thread.Sleep(3000);
+        }
+        public void FolderSharedThreeDotsMenu()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            string folderName = this.FolderCreated();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("QCB1_Button1")));
+            //allPagesButton.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath($"//a[starts-with(@aria-label,'{folderName}')]")));
+            IWebElement page = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{folderName}')]")));
+            Actions action = new Actions(driver);
+            IWebElement row = wait.Until(x => x.FindElement(By.XPath($"//a[starts-with(@aria-label,'{folderName}')]/../../..")));
+            IWebElement threeDotsButton = row.FindElement(By.XPath(".//a[contains(@id,'calloutLaunchPoint')]"));
+            threeDotsButton.Click();
+            IWebElement threeDotsFromPopUp = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".js-callout-ecbActionDownArrow")));
+            threeDotsFromPopUp.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("ms-contextmenu-list")));
+            IWebElement menu = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//li[@aria-label='Share']")));
+            action.MoveToElement(menu);
+            menu.Click();
+            IWebElement peoplePickerDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan")));
+            action.MoveToElement(peoplePickerDiv);
+            peoplePickerDiv.Click();
+            IWebElement peoplePickerInput = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_EditorInput")));
+            action.MoveToElement(peoplePickerInput);
+            Thread.Sleep(500);
+            peoplePickerInput.SendKeys("mzywicki");
+            IWebElement peoplePickerResultContainer = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("peoplePicker_TopSpan_AutoFillDiv")));
+            peoplePickerResultContainer.Click();
+            shareButton.Click();
             Thread.Sleep(3000);
         }
     }
